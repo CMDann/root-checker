a3d5520492007d5b6352c05352aaa2f3aaaa7ed2
Daniel Blair <CMDann.Development@gmail.com>
2013-05-11 01:43:00 -0500

    Added additional functionality
    Added sections for device information including model, android version
    and code name.

--- RootChecker/src/com/cmdann/rootchecker/MainActivity.java
+++ RootChecker/src/com/cmdann/rootchecker/MainActivity.java
@@ -1,6 +1,7 @@
 package com.cmdann.rootchecker;
 
 import android.app.Activity;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.Menu;
@@ -17,6 +18,12 @@ public class MainActivity extends Activity {
 
 		final Button button1 = (Button) findViewById(R.id.button1);
 		final Button button2 = (Button) findViewById(R.id.button2);
+
+		// Device text views
+		final TextView Text1 = (TextView) findViewById(R.id.textView5);
+		final TextView Text2 = (TextView) findViewById(R.id.TextView09);
+		final TextView Text3 = (TextView) findViewById(R.id.TextView06);
+		final TextView Text4 = (TextView) findViewById(R.id.TextView05);
 
 		button1.setOnClickListener(new View.OnClickListener() {
 
@@ -37,7 +44,24 @@ public class MainActivity extends Activity {
 					button2.setBackgroundResource(R.drawable.xmark);
 				}
 				// setNewTextInTextView(text); //Debugging
+				// The end-user-visible name for the end product.
+				String curModel = Build.MODEL;
+				// The name of the overall product.
+				String curProduct = Build.PRODUCT;
+				// The manufacturer of the product/hardware.
+				String curMan = Build.MANUFACTURER;
+				// The brand (e.g., carrier) the software is customized for, if
+				// any.
+				String curBrand = Build.BRAND;
+				// The name of the industrial design.
+				String curDevice = Build.DEVICE;
+				String curVer = Build.VERSION.RELEASE;
 
+				// Set the current information
+				Text1.setText(curDevice);
+				Text2.setText(curModel);
+				Text3.setText(curMan);
+				Text4.setText(curVer);
 			}
 		});
 	}
